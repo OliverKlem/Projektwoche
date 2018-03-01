75485fb9edf52be3ec7f2c3a716e3de0c9f926be
SteWar <Steffi.20@t-online.de>
2018-03-01 09:56:05 +0100

    1

--- Serverueberwachung/Serverueberwachung_Server/Serverueberwachung_Server.cs
+++ Serverueberwachung/Serverueberwachung_Server/Serverueberwachung_Server.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
+using System.Management;
+using System.Diagnostics;
 
 namespace Serverueberwachung_Server
 {
@@ -21,13 +23,21 @@ namespace Serverueberwachung_Server
             InitializeComponent();
 
         }
+        PerformanceCounter perfCPUCounter = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+        PerformanceCounter perfMemCounter = new PerformanceCounter("Memory", "Available MBytes");
+        PerformanceCounter perfSystemCounter = new PerformanceCounter("System", "System Up Time");
 
+
+
+     
         private void Serverueberwachung_Server_Load(object sender, EventArgs e)
         {
             server = new AsyncTcpServer(IPAddress.Parse("127.0.0.1"), 49001);
             server.Start();
             server.PacketReceived += Server_PacketReceived;
             server.ClientConnected += Server_ClientConnected;
+            timer1.Start();
+           // GetComponent();
         }
 
         private void Server_PacketReceived(object sender, PacketReceivedEventArgs e)
@@ -37,5 +47,12 @@ namespace Serverueberwachung_Server
         private void Server_ClientConnected(object sender, ClientConnectedEventArgs e)
         {
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Text = "CPU Auslastung:" + "   " + (int)perfCPUCounter.NextValue() + "   " + "%";
+            label2.Text = "Verfügbarer RAM" + "  " + (int)perfMemCounter.NextValue() + "  " + "MB";
+            label3.Text = "Betriebszeit" + "  " + (int)perfSystemCounter.NextValue() / 60 / 60 + " Stunden" ;  
+                }
     }
 }
